1a41e312e4e71a5b11d2593731309641abe89962
Nesar Wagannawar <nesar2004@gmail.com>
2025-01-23 22:27:13 +0530

    Implement unique room ID generation and add room ID regeneration feature; enhance participant management on host disconnection

--- server/server.js
+++ server/server.js
@@ -12,10 +12,20 @@ app.use(cors());
 
 let connectedUsers = [];
 let rooms = [];
+let previousRoomIDs = new Set();
 
 // Function to generate a 9-character alphanumeric room ID
 const generateRoomID = () => {
     return Math.random().toString(36).substring(2, 11).toUpperCase();
+};
+
+const generateUniqueRoomID = () => {
+    let roomID;
+    do {
+        roomID = generateRoomID();
+    } while (rooms.find(room => room.roomID === roomID) || previousRoomIDs.has(roomID));
+    previousRoomIDs.add(roomID);
+    return roomID;
 };
 
 app.get('/api/rooms-exists/:roomID', (req, res) => {
@@ -43,7 +53,7 @@ io.on('connection', (socket) => {
     console.log(`A user connected ${socket.id}`);
 
     socket.on('create-new-room', (data) => {
-        const roomID = generateRoomID();
+        const roomID = generateUniqueRoomID();
         socket.join(roomID);
         console.log(`Room created with ID: ${roomID}`);
         rooms.push({ roomID, participants: [{ socketID: socket.id, identity: data.identity }], host: socket.id });
@@ -51,6 +61,25 @@ io.on('connection', (socket) => {
         console.log('Connected Users:', connectedUsers);
         socket.emit('room-created', { roomID });
         io.to(roomID).emit('update-participants', rooms.find(room => room.roomID === roomID).participants);
+    });
+
+    socket.on('regenerate-room-id', () => {
+        const user = connectedUsers.find(user => user.socketID === socket.id);
+        if (user) {
+            const oldRoomID = user.roomID;
+            const room = rooms.find(room => room.roomID === oldRoomID);
+            if (room && room.host === socket.id) {
+                const newRoomID = generateUniqueRoomID();
+                room.roomID = newRoomID;
+                user.roomID = newRoomID;
+                socket.leave(oldRoomID);
+                socket.join(newRoomID);
+                previousRoomIDs.add(newRoomID);
+                console.log(`Room ID regenerated from ${oldRoomID} to ${newRoomID}`);
+                io.to(oldRoomID).emit('room-id-regenerated', { newRoomID });
+                io.to(newRoomID).emit('update-participants', room.participants);
+            }
+        }
     });
 
     socket.on('join-room', (data) => {
@@ -72,6 +101,27 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on('leave-room', () => {
+        const user = connectedUsers.find(user => user.socketID === socket.id);
+        if (user) {
+            const roomID = user.roomID;
+            connectedUsers = connectedUsers.filter(user => user.socketID !== socket.id);
+            console.log('Remaining Connected Users:', connectedUsers);
+            const room = rooms.find(room => room.roomID === roomID);
+            if (room) {
+                room.participants = room.participants.filter(participant => participant.socketID !== socket.id);
+                if (room.host === socket.id) {
+                    // If the host leaves, assign a new host
+                    if (room.participants.length > 0) {
+                        room.host = room.participants[0].socketID;
+                        io.to(roomID).emit('new-host', { newHostID: room.host });
+                    }
+                }
+                io.to(roomID).emit('update-participants', room.participants);
+            }
+        }
+    });
+
     socket.on('disconnect', () => {
         console.log(`User disconnected ${socket.id}`);
         const user = connectedUsers.find(user => user.socketID === socket.id);
@@ -83,15 +133,13 @@ io.on('connection', (socket) => {
             if (room) {
                 room.participants = room.participants.filter(participant => participant.socketID !== socket.id);
                 if (room.host === socket.id) {
-                    // If the host leaves, remove the room and notify all participants
-                    rooms = rooms.filter(room => room.roomID !== roomID);
-                    io.to(roomID).emit('room-closed');
-                    io.in(roomID).socketsLeave(roomID); // Disconnect all users from the room
-                } else if (room.participants.length > 0) {
-                    io.to(roomID).emit('update-participants', room.participants);
-                } else {
-                    rooms = rooms.filter(room => room.roomID !== roomID);
+                    // If the host leaves, assign a new host
+                    if (room.participants.length > 0) {
+                        room.host = room.participants[0].socketID;
+                        io.to(roomID).emit('new-host', { newHostID: room.host });
+                    }
                 }
+                io.to(roomID).emit('update-participants', room.participants);
             }
         }
     });
